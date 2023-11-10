13ce9c2132efad6850611c65a92be2b39e9d620d
PHUONGSANHMINH <minhps.22itb@vku.udn.vn>
2023-11-10 21:31:14 +0700

    test

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { useEffect, useRef, useState } from 'react'  
+import { FadeInAnimation } from './animation';
+
+function Welcome(){
+  const ref = useRef(null);
+
+  useEffect(()=>{
+    const animation = new FadeInAnimation(ref.current);
+    animation.start(1000);
+    return () =>{
+      animation.stop();
+    };
+  }, []);
+  return (
+    <h1
+      ref = {ref}
+      style={{
+        opacity: 0,
+        color: 'white',
+        padding: 50,
+        textAlign: 'center',
+        fontSize: 50,
+        backgroundImage: 'radial-gradient(circle, rgba(63,94,251,1) 0%, rgba(252,70,107,1) 100%)'
+      }}
+    >
+      CHUẨN BỊ ĐỘI QUẦN ĐI THÚY
+    </h1>
+  )
+}
+  export default function App(){
+    const [show, setShow] = useState(false);
+    return (
+      <>
+        <button onClick={() => setShow(!show)}>
+          {show ? 'Remove' : 'Show'}
+        </button>
+        <hr />
+        {show && <Welcome/>}
+      </>
+    )
+  }
